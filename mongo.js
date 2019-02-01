bdcc5e3ad9611a01cd95ffa7deadb876af5adbf1
Aaron Newell <user@Aarons-MacBook-Air-2.local>
2019-02-01 16:47:31 +0100

    Updated promises section of mongo.

--- mongo.js
+++ mongo.js
@@ -33,53 +33,39 @@ MongoClient.connect(url, function(error, mongo) {
    // If no error, let's use the `mongo` object.
    // We'll use to access the database.
    //
-   // We're going to pass that object to a function
-   // That we will define below
-   useTheMongoDatabse(mongo)
+   // We're going to access the my_recap_database database, and the my_recap_collection collection.
+   // If they don't exist, Mongo will create them for us.
+   // 
+   // And we're going to use another callback function, addToCollection function, which we
+   // will define later.
+    mongo.db("my_recap_database").collection("my_recap_collection", addToCollection)
  } 
 })
 
-function useTheMongoDatabse(mongo) {
-  // We're now going to look at a database name
-  // And a collection name.
-  // If they don't exist, Mongo will create them for us.
-  //
-  // We will run mongo.db("my_recap_database").collection("my_recap_collection", onConnected)
-  // This will eventually run the onConnected function, which we will now create.
-  //
-  // In this function, we will add something to our database. Let's create that now.
-  //
-  var onConnected = function(error, collection) {
-    // Mongo will call this function. 
-    if(error) {
-      // If there's been an error, the 'error' object will have something in it
-      console.log("Error opening the database and collection", error)
-    } else {
-      // Otherwise, let's use the 'collection' object to insert a random javascript object (JSON)
-      // into our database. We'll define another function for this.
-      addDataToDatabase(collection)
+function addToCollection(error, collection) {
+  // Mongo will call this function, since it's our callback
+  // It will pass a error object (if there's one)
+  // or `collection` if it found the collection
+  if(error) {
+    // If there's been an error, the 'error' object will have something in it
+    console.log("Error opening the database and collection", error)
+  } else {
+    // We're defining some arbitary javascript object
+    var country_data = {
+      "country" : "scotland",
+      "population" : "3000000"	
     }
+    // Then we're going to insert it
+    collection.insertOne(country_data, function(error, result) {
+      // This function will be run when we've inserted something.
+      // Error will be something if there's been an error
+      if(error) {
+        console.log("Error inserting something into my_recap_collection", error)
+      } else {
+        console.log("In non promise: Successfully added something into my_recap_collection!")
+      }
+    })
   }
-  // Now we've defined our callback function, let's pass 
-  // it to Mongo, pointing at our database and our collection
-  mongo.db("my_recap_database").collection("my_recap_collection", onConnected)
-}
-
-function addDataToDatabase(collection) {
-  // We've passed in the collection we got from connecting to the database.
-  var country_data = {
-    "country" : "scotland",
-    "population" : "3000000"	
-  }
-  collection.insertOne(country_data, function(error, result) {
-    // This function will be run when we've inserted something.
-    // Error will be something if there's been an error
-    if(error) {
-      console.log("Error inserting something into my_recap_collection", error)
-    } else {
-      console.log("Successfully added something into my_recap_collection!")
-    }
-  })
 }
 
 // #########
@@ -90,11 +76,11 @@ function addDataToDatabase(collection) {
 
 MongoClient.connect(url)
 .then(function(mongo) {
-  console.log("Hooray we've connected to the database")
+  console.log("In promise: Hooray we've connected to the database")
   return mongo.db("my_recap_database").collection("my_recap_collection")
 })
 .then(function(collection) {
-  console.log("Hooray we've connected to the collection")
+  console.log("In promise: Hooray we've connected to the collection")
   var country_data = {
     "country" : "scotland",
     "population" : "3000000"	
@@ -102,8 +88,8 @@ MongoClient.connect(url)
   return collection.insertOne(country_data)
 })
 .then(function(goodResult) {
-   console.log("We've added something to the database!")
+   console.log("In promise: We've added something to the database!")
 })
 .catch(function(error) {
-   console.log("Some kind of monogo error", error)
+   console.log("In promise: Some kind of monogo error", error)
 })
