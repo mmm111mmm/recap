3ccc9c03802bd5f75166da35b650ee7f7970efda
newfivefour <newfivefour@newfivefour.com>
2019-02-03 14:26:42 +0100

    Updated the mongo.js file.

--- mongo.js
+++ mongo.js
@@ -12,7 +12,7 @@
 //
 // #########
 // #
-// # The section deals with using Mongo without Mongoose with Javascript Promises
+// # The section deals with using Mongo without Mongoose and with Javascript Promises
 // #
 // #########
 
@@ -29,13 +29,13 @@ var url = "mongodb://localhost:27017/";
 // Then we'll use it to find, add, delete etc from the collection
 var songsCollection; 
 
-MongoClient.connect(url)
+MongoClient.connect(url, { useNewUrlParser: true })
 .then(function(mongo) {
-  console.log("Hooray we've connected to the database")
+  //console.log("Hooray we've connected to the database")
   return mongo.db("my_recap_database").collection("my_recap_songs")
 })
 .then(function(collection) {
-  console.log("Hooray we've connected to the collection")
+  //console.log("Hooray we've connected to the collection")
   songsCollection = collection; // save it from later use
   var song_data = {
     "song_title" : "Gimme Shelter",
@@ -44,14 +44,15 @@ MongoClient.connect(url)
   return collection.insertOne(song_data)
 })
 .then(function(goodResult) { // Mongo gives us a object from insertOne's promise
-  console.log("We've added something to the database!")
+  //console.log("We've added something to the database!")
   // This returns a promise that looks at all the data in our collection 
   // We have to say "toArray" so it gives us an array
   // And we're using the songsCollection we previously saved
   return songsCollection.find().toArray() 
 })
 .then(function(songsResult) { // Mongo gives us the results from find()'s promise
-  console.log("Here are the results", songsResult)
+  //console.log("We got the results")
+  // We could print out songResult to see them all
 })
 .catch(function(results) {
    console.log("Some kind of monogo error", results)
@@ -71,7 +72,9 @@ MongoClient.connect(url)
 
 // #########
 // #
-// # The section deals with using Mongo with Mongoose
+// # The section deals with using Mongo with Mongoose.
+// # It only deals with creating new records. We'll deal
+// # with updating, deleting and fetching records in the next file.
 // #
 // # This relates to the lecture: http://learn.ironhack.com/#/learning_unit/6481
 // #
@@ -81,7 +84,7 @@ MongoClient.connect(url)
 // And this is called Mongoose. It has many features.
 //
 // But for the moment we'll just focus on connecting to our 
-// database and collection, adding something and fetching those things again.
+// database and collection adding records to that collection.
 //
 // We'll talk about its advanced features, include the Schema in more default, later.
 //
@@ -90,7 +93,9 @@ MongoClient.connect(url)
 // Then we must include it:
 const mongoose = require('mongoose');
 // Then connect to our database, and you also specify the database name
-mongoose.connect('mongodb://localhost/my_recap_database');
+// We also say { useNewUrlParser: true } to tell Mongo we're using the URL format 
+// (which we are).
+mongoose.connect('mongodb://localhost/my_recap_database', { useNewUrlParser: true });
 //
 // Mongoose uses something called a Model.
 // This is an Javascript object that relates to a Mongo collection.
@@ -127,13 +132,13 @@ const Song = mongoose.model('my_recap_songs', songSchema);
 
 Song.create({ song_title: "Rocky Raccoon", artist: "The Beatles"} )
 .then(function(success) {
-  console.log("Success adding with create", success)
+  console.log("Success adding with Mongoose's create")
 })
 .catch(function(error) {
   console.log("Error adding", error)
 });
 
-// You can create many objects in a similar way
+// You can create many objects with `create` too.
 
 var songs = [
   { song_title: "So what", artist: "Miles Davis" },
@@ -142,21 +147,62 @@ var songs = [
 
 Song.create(songs)
 .then(function(success) {
-  console.log("Success adding with create", success)
+  console.log("Success adding with Mongoose's create (adding multiple songs)" )
 })
 .catch(function(error) {
   console.log("Error adding", error)
 });
 
-// insert many
+// You can also use insertMany for the same thing
+
+Song.insertMany(songs)
+.then(function(success) {
+  console.log("Success adding with Mongoose's insertMany")
+})
+.catch(function(error) {
+  console.log("Error adding", error)
+});
+
+// TODO: Using 'new'
+
+// TODO: Promises are not sequential.
+
+// Let's talk about unique ids.
+
+// For each document ( i.e. the song) we entered, mongo did something automatically for us
+// Mongo gave us an _id field. 
+// 
+// Look into Mongo compass for the documents we added, and you will see it.
+//
+// This _id field has an automatically generated and unique id. 
+// We will use this later to uniquely refer to a document.
+
+
+
+
+
+
+
+// If you want to find all our songs you can do
+/*
+Song.find()
+.then(function(songs) {
+  console.log("Success getting all our songs via Mongoose's find")
+  songs.forEach(function(song) {
+    console.log("I found a Song via Mongoose's find:")
+    console.log(song)
+  }) 
+})
+.catch(function(error) {
+  console.log("Error adding", error)
+});
+*/
+
+// More about find
 
 // delete
 
-// find
-
 // update
-
-// find
 
 // There is a reference of all the possible calls here: https://mongoosejs.com/docs/api.html#Model
 
