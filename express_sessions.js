8fa43f4b9e17d2ca80f28bc007d84d24a4ec990e
newfivefour <newfivefour@newfivefour.com>
2019-02-07 02:33:37 +0100

    Finished the express sessions file and comments.

--- express_sessions.js
+++ express_sessions.js
@@ -1,11 +1,75 @@
-// WORK IN PROGRESS
+// ##########################
+// # What's a Session?
+// ##########################
 
-// THIS SHOWS YOU HOW TO USE EXPRESSJS SESSIONS
-// TO KEEP USER SPECIFIC DATA.
-//
-// There's nothing about password or user accounts yet.
-//
-// That will come next.
+// We need a way to remember the user when they 
+// come back to our website.
+
+// For example, Amazon remembers it's you when you
+// reopen Amazon. (If you've logged in first)
+
+// A Session is a way for the server to remember the user.
+
+// And a Session is a way to keep information about the user e.g.
+// Theser's name is "Aaron". Etc.
+// It stores this as a normal javascript object.
+
+// ##########################
+// # What's a Cookie?
+// ##########################
+
+// A Cookie is a small text file the server gives the client's browser.
+// It's stored in the user's web browser.
+
+// This cookie has a long random string in it, like this: 4DFJALIO6KDFNA
+
+// And everytime the user uses your website the user's browser
+// sends this cookie (and that long random string) to your nodejs server.
+
+// So the server can use that Cookie to know it's you.
+
+// ##########################
+// # How do Sessions and Cookies work togther?
+// ##########################
+
+// When you setup Express and Sessions (which we'll do below)
+// then Express will send a Cookie to the user.
+
+// Subsequently, every time the user uses your website they'll
+// send that Cookie back to Express.
+
+// Express will say 
+// "Ah, I know Cookie 4DFJALIO6KDFNA!"
+// "I know that Cookie and I have Session object for it.
+//  I have stored the Session object in Mongo.
+//  And that javascript object is:
+//  { name: "Aaron" } etc
+
+// That javascript object is called the Session object
+// and it lives in `request.session` in the routes
+
+// You can, then, modify that javascript object.
+// For example, you can destroy it when the user
+// logs out, or modify it to remember items the
+// user has added to a shopping basket.
+
+
+// #####################
+// # 
+// # Let's make a simple app with Sessions.
+// #
+// # * When the user goes to the Login page, they will
+// #   input their username (no passwords in this example)
+// #
+// # * We will then remember their username in our session.
+// # 
+// # * Then when they go to the main page, we'll display that
+// #   username 
+// #
+// # * Even if they close the browser and open it again their
+// #   session will still be there - we'll still remmeber them.
+// #####################
+
 
 // #####################
 // # Setup Mongo, Mongoose, Sessions and Express
@@ -14,9 +78,13 @@
 // This means we must run 'npm install express express-session mongoose connect-mongo'
 const express      = require('express')
 const app          = express()
+const bodyParser   = require('body-parser');
 const mongoose     = require("mongoose");
+// This allows us to use sessions
 const session      = require("express-session");
+// We'll store our session objects in Mongo
 const ConnectMongo = require("connect-mongo");
+// Specifically, we'll sotre our session objects via MongoStore
 const MongoStore   = ConnectMongo(session);
 
 // Let's connected to a Mongo database
@@ -24,14 +92,14 @@ const MongoStore   = ConnectMongo(session);
 mongoose.connect('mongodb://localhost/my_recap_database', {useNewUrlParser: true})
 
 // Let's use Mongo to store our session.
-// We'll create a MongoStore which EspressJS
+// We'll create a 'MongoStore' which EspressJS
 // will use below.
 
 // We give MongoStore our Mongoose connection that
 // we started above.
 //
 // And we give it a Time To Life, which means
-// long long this should live before it's 
+// how long this should live before it's 
 // destroyed.
 //
 // We don't want to keep around millions of 
@@ -56,60 +124,80 @@ var mongoStore = new MongoStore({
 // We tell the browser to delete the cookie after 60000 seconds
 //
 // The 'secret' is a arbitrary string that 
-// helps keep the session key random.
+// helps keep the session key random - so it's not gussable
+// You can put anything here.
 
 app.use(session({
-  secret: "basic-auth-secret",
+  secret: "some random stuff - put anything here",
   cookie: { maxAge: 60000 }, 
   store: mongoStore
 }))
 
+app.use(bodyParser.urlencoded({ extended: true }));
 // Start ExpressJS
-
 app.listen(3000, function() {
   console.log('My app listening on port 3000!')
 })
 
+// #####################
+// # Show the user a HTML
+// # form where they enter their username.
+// # They are sent to our 'login_post' route.
+// #####################
+
+app.get("/login", function(request, response, next) {
+  response.send(`
+    <form action="login_post" method="post">
+      <input name="username" placeholder="username">
+      <button type="submit">login</button>
+    </form>
+  `);
+});
+
+// Once the user clicks submit on the HTML form above
+// then we end up here.
+// And we save the data from the POST body to our session
+// as the object 'currentUser'.
+
+app.post("/login_post", function(request, response, next) {
+
+  request.session.currentUser = {
+    username: request.body.username
+  }
+  
+  response.redirect("/")
+});
 
 // #####################
-// # A route to show login and logout buttons
-// # It will also show a link to the 'secret' page
+// # We look to see if we have the 
+// # 'currentUser' object in our session.
+// # (We create 'currentUser' in our login POST route)
+// # If so, let's display their username.
 // #####################
 
 app.get('/', function(request, response, next) { 
-
-  if(request.session.currentUser == undefined) {
-    request.session.currentUser = { oranges: 0 }
-  }
   
+  var username = "Unknown user"
+  if(request.session.currentUser) {
+    username = request.session.currentUser.username
+  }
+
   response.send(`
-    <div>You have ${request.session.currentUser.oranges} in your basket</div>
-    <hr>
-    <a href="/add_to_basket">buy an orange</a>
-    <hr>
+    <a href="/login">login</a>
     <a href="/logout">logout</a>
+    <hr>
+    <div>Welcome, ${username}</div>
   `)
 });
 
 // #####################
-// # Here's the route that allows
-// # you to modify the user session
-// #####################
-
-app.get("/add_to_basket", function(request, response, next) {
-  request.session.currentUser.oranges++;
-  response.redirect("/");
-});
-
-// #####################
-// # Here's the route that destroys
-// # all the data in the user session.
-// # Or 'logout' as we normally call it.
+// # Let's destroy the session
+// # And thus the 'currentUser' session object.
 // #####################
 
 app.get('/logout', function(request, response, next) { 
   request.session.destroy(function(error) {
-    console.log("Couldn't destroy the sesson", error)
+    if(error) console.log("Couldn't destroy the sesson")
   })
   response.redirect("/")
 });
---
+++ express_sessions_pw_bcrypt.js
@@ -0,0 +1,160 @@
+
+// #####################
+// # Setup Mongo, Mongoose, Sessions and Express
+// #####################
+
+// This means we must run 'npm install express express-session mongoose connect-mongo'
+const express      = require('express')
+const app          = express()
+const bodyParser   = require('body-parser');
+const mongoose     = require("mongoose");
+const session      = require("express-session");
+const ConnectMongo = require("connect-mongo");
+const bcrypt       = require("bcrypt")
+const MongoStore   = ConnectMongo(session);
+
+mongoose.connect('mongodb://localhost/my_recap_database', {useNewUrlParser: true})
+var mongoStore = new MongoStore({ 
+  mongooseConnection: mongoose.connection,
+  ttl: 24 * 60 * 60
+})
+
+app.use(session({
+  secret: "some random stuff - put anything here",
+  cookie: { maxAge: 60000 }, 
+  store: mongoStore
+}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(3000, function() {
+  console.log('My app listening on port 3000!')
+})
+
+
+const userSchema = { username: String, password: String };
+const User = mongoose.model('my_recap_users_encrypted', userSchema);
+
+
+// #####################
+// # 
+// #####################
+
+app.get("/register", function(request, response, next) {
+  response.send(`
+    <h1> Register </h1>
+    <form action="register_post" method="post">
+      <input name="username" placeholder="username">
+      <input name="password" placeholder="password">
+      <button type="submit">register</button>
+    </form>
+  `);
+});
+
+// #####################
+// # 
+// #####################
+
+app.post("/register_post", function(request, response, next) {
+
+  const salt     = bcrypt.genSaltSync(10);
+  const hashPass = bcrypt.hashSync(request.body.password, salt);
+
+  var user = {
+    username: request.body.username, 
+    password: hashPass
+  }
+
+  User.create(user)
+  .then(function(success) {
+     console.log("Successfully added a user", success)
+     response.redirect("/")
+  })
+  .catch(function(error) {
+    console.log("Error adding the user", error)
+    response.send(`
+      <h1>Error adding the user</h1>
+    `)
+  })
+  
+});
+
+// #####################
+// #
+// #####################
+
+app.get("/login", function(request, response, next) {
+  response.send(`
+    <h1> Login </h1>
+    <form action="login_post" method="post">
+      <input name="username" placeholder="username">
+      <input name="password" placeholder="password">
+      <button type="submit">login</button>
+    </form>
+  `);
+});
+
+// #####################
+// # 
+// #####################
+
+app.post("/login_post", function(request, response, next) {
+
+  var attemptedLogin = {
+    username: request.body.username
+  }
+
+  User.findOne(attemptedLogin)
+  .then(function(userFromMongo) {
+    console.log("Found this user on login attempt", userFromMongo)
+    if(userFromMongo == null) {
+      response.send(`
+        Couldn't find that user.
+      `)
+    } else {
+
+      var goodPassword = bcrypt.compareSync(request.body.username, userFromMongo.password)
+
+      console.log("Good pass?", goodPassword)
+
+      request.session.currentUser = userFromMongo
+      response.redirect("/")        
+    }
+  })
+  .catch(function() {
+    console.log("Not found this user")
+    response.send(`
+      Error finding that user.
+    `)
+  })
+  
+});
+
+// #####################
+// # 
+// #####################
+
+app.get('/', function(request, response, next) { 
+  
+  var areLoggedIn = "You are not logged in."
+  if(request.session.currentUser) {
+    areLoggedIn = "You are: " + request.session.currentUser.username
+  }
+
+  response.send(`
+    <a href="/login">login</a>
+    <a href="/logout">logout</a>
+    <a href="/register">register</a>
+    <hr>
+    <div><b>${areLoggedIn}</b></div>
+  `)
+});
+
+// #####################
+// # 
+// #####################
+
+app.get('/logout', function(request, response, next) { 
+  request.session.destroy(function(error) {
+    if(error) console.log("Couldn't destroy the sesson")
+  })
+  response.redirect("/")
+});
